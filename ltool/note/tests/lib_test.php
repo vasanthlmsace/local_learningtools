c5e30c0ebe5ec3724d21a5150c24e62737b6c0d7
prasanna lmsace <prasanna.lmsace@gmail.com>
2021-08-23 16:10:52 +0530

    Test cases updated & CI updated

--- ltool/note/tests/lib_test.php
+++ ltool/note/tests/lib_test.php
@@ -65,16 +65,6 @@ class ltool_note_testcase extends advanced_testcase {
         return $data;
     }
 
-    public function test_get_contextuser_notes() {
-        global $DB;
-        $toolobj = new \ltool_note\note();
-        $tool = $DB->get_record('local_learningtools_products', ['shortname' => 'note']);
-        $data = $this->get_note_info($toolobj, $tool);
-        $test = get_contextuser_notes($data);
-        print_r($records);
-        $this->assertCount(1, $test);
-    }
-
     /**
      * Test the note save method and the triggered event have correct context.
      *
