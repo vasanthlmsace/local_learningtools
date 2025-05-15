2974df14b775323579dbf6b6c4cc96718640363a
vasanthlmsace <vasanthr.lmsace@gmail.com>
2025-05-15 12:22:34 +0530

    Update the source.

--- version.php
+++ version.php
@@ -24,9 +24,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_learningtools';
-$plugin->version = 2024120300;
+$plugin->version = 2025042902;
 $plugin->release = 'v1.1.1';
 $plugin->requires = 2022041900; // Moodle 4.0.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->supported = [400, 405];
-
